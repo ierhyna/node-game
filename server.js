cc7d51225354702145c7163ad6161638e996acbc
Irina Sokolovskaya <irina.sokolovskaya@dataart.com>
2017-05-15 14:57:03 +0300

    Serve assets

--- server.js
+++ server.js
@@ -2,3 +2,15 @@ const express = require('express');
 const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io').listen(server);
+
+app.use('/css', express.static(__dirname + '/css'));
+app.use('/js', express.static(__dirname + '/js'));
+app.use('/assets', express.static(__dirname + '/assets'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+server.listen(8082, () => {
+  console.log('Listening on ' + server.address().port);
+});
