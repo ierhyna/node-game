06844b3d48d9bdf7d0fe7d35c28520d33e92c3c5
Irina Sokolovskaja <sokolovskaja.irina@gmail.com>
2017-05-15 23:09:46 +0300

    Fix game object

--- js/game.js
+++ js/game.js
@@ -3,19 +3,19 @@ import 'p2';
 import Phaser from 'phaser';
 import Client from './client.js';
 
+const Game = {
+  preload: function() {},
+  create: function() {
+    game.stage.disableVisibilityChange = true;
+    // game.physics.enable(player, Phaser.Physics.ARCADE);
+    Client.sendTest();
+    console.log("ran")
+  },
+  update: function() {}
+};
+
 const game = new Phaser.Game(16*32, 600, Phaser.AUTO, document.getElementById('game'));
 game.state.add('Game', Game);
 game.state.start('Game');
-
-const Game = {};
-
-Game.init = () => {
-  game.stage.disableVisibilityChange = true;
-};
-
-Game.create = () => {
-  game.physics.enable(player, Phaser.Physics.ARCADE);
-  Client.sendTest();
-};
 
 export default Game;
