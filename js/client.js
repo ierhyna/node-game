2b799145d9d068c1edd51163b4b813ca52b6f4eb
Alex Bykov <spbeat@mail.ru>
2017-05-23 23:24:21 +0300

    sprites

--- js/client.js
+++ js/client.js
@@ -1,36 +1,35 @@
-import {
-    Game
-} from './stages/Game.stage';
+import { Game } from "./stages/Game.stage";
 
 const Client = {};
-Client.socket = io.connect('http://localhost:8082/');
+Client.socket = io.connect("http://localhost:8082/");
 
 Client.addPlayer = () => {
-    Client.socket.emit('newPlayer');
+    Client.socket.emit("newPlayer");
 };
 
-Client.socket.on('newPlayerConnected', (player) => {
+Client.socket.on("newPlayerConnected", player => {
     console.log(`Player ${player.id} connected`);
     Game.renderNewPlayer(player.id, player.x, player.y);
 });
 
-Client.socket.on('playersRerender', (players) => {
+Client.socket.on("playersRerender", players => {
     players.forEach(player => {
         Game.renderNewPlayer(player.id, player.x, player.y);
     });
 });
 
-Client.socket.on('remove', (id) => {
+Client.socket.on("remove", id => {
     Game.removePlayer(id);
 });
 
-Client.socket.on('receiveMove', data => {
-    console.log('client got ', data);
-    Game.move(data)})
+Client.socket.on("receiveMove", data => {
+    console.log("client got ", data);
+    Game.move(data);
+});
 
-Client.move = (data) => {
-    console.log('randomizing')
-    Client.socket.emit('sendMove', data);
-}
+Client.move = data => {
+    console.log("randomizing");
+    Client.socket.emit("sendMove", data);
+};
 
 export default Client;
