1849964c540cef5b9fdc9de2af7d96bf967e74a4
Alex Bykoff <spbeat@mai.ru>
2017-05-31 21:06:45 +0300

    name tags

--- js/stages/Game.stage.js
+++ js/stages/Game.stage.js
@@ -2,12 +2,14 @@ import game from "../game";
 import Client from "../client";
 
 let cursors;
+let nameTag;
 let velocity = {
     x: 0,
     y: 0
 };
 
 export const Game = {
+
     preload: function () {
         game.load.tilemap(
             "room",
@@ -18,40 +20,41 @@ export const Game = {
         game.load.image("tileset", "assets/maps/tilea2.png");
         game.load.image("sprite", "assets/sprites/char.gif");
     },
+
     create: function () {
         game.stage.disableVisibilityChange = true;
         Game.playerMap = {};
         Game.tileMap = game.add.tilemap("room");
         Game.tileMap.addTilesetImage("tilea2", "tileset");
         Game.bgLayer = Game.tileMap.createLayer("layer_0");
-
         Client.addPlayer();
         cursors = game.input.keyboard.createCursorKeys();
     },
+
     update: function () {
-        console.log(Game.playerMap[Client.socket.id])
         if (!Game.playerMap[Client.socket.id]) return;
+        nameTag.x = Game.playerMap[Client.socket.id].x + Game.playerMap[Client.socket.id].width / 2;
+        nameTag.y = Game.playerMap[Client.socket.id].y - 14;
         velocity.x = 0;
         velocity.y = 0;
 
         if (cursors.up.isDown) {
-            velocity.y = -120;
+            velocity.y = -150;
         }
         if (cursors.down.isDown) {
-            velocity.y = 120;
+            velocity.y = 150;
         }
         if (cursors.left.isDown) {
-            velocity.x = -120;
+            velocity.x = -150;
         }
         if (cursors.right.isDown) {
-            velocity.x = 120;
+            velocity.x = 150;
         }
-
         if (Game.playerMap[Client.socket.id]) {
             Game.playerMap[Client.socket.id].body.velocity.x = velocity.x;
             Game.playerMap[Client.socket.id].body.velocity.y = velocity.y;
-
         }
+
         Client.updatePositions({
             id: Client.socket.id,
             x: Game.playerMap[Client.socket.id].body.position.x,
@@ -60,15 +63,27 @@ export const Game = {
     },
     renderNewPlayer: function (id, x, y) {
         Game.playerMap[id] = game.add.sprite(x, y, "sprite");
-        Game.playerMap[id].scale.setTo(0.25, 0.25);
-        game.physics.arcade.enable(Game.playerMap[id]);
-        Game.playerMap[id].body.enable = true;
-        Game.playerMap[id].body.collideWorldBounds = true;
+        const _p = Game.playerMap[id];
+        _p.scale.setTo(0.25, 0.25);
+        game.physics.arcade.enable(_p);
+        _p.body.enable = true;
+        _p.body.collideWorldBounds = true;
+        if (!nameTag) {
+            // we create name Tag only if it does not exist yet;
+            nameTag = game.add.text(0, 0, (Client.socket.id).slice(0, 10), {
+                font: "14px Arial",
+                fill: "#fff",
+                align: "center"
+            });
+            nameTag.anchor.setTo(0.5, 0.5);
+        }
     },
+
     removePlayer: function (id) {
         Game.playerMap[id].destroy();
         delete Game.playerMap[id];
     },
+    
     move: function (data) {
         Game.playerMap[data.id].y = data.y;
         Game.playerMap[data.id].x = data.x;
